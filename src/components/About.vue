a3e53dd62f9db8b6c0e30ca0632736fbc286b89c
wish911wish <wish911wish@gmail.com>
2019-11-12 01:57:41 +0900

    fix about component

---
+++ src/components/About.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="works">
+    <div class="contentsTtl">
+      <h1>About</h1>
+      <h5>My career and skills.</h5>
+    </div>
+    <div class="contentsInner">
+      <div class="articles">
+        <div class="article works">
+          <h2 class="articleTtl">
+            Career
+          </h2>
+          <div class="articleDescription description">
+            <h4 class="mb-1">
+              株式会社アグレックス : 2016.8 ~ now
+            </h4>
+            <p>
+              金融系業務アプリケーションの開発・保守を担当。<br/>１〜２年目は某信託銀行に常駐し、EUCアプリケーションの開発業務の要件定義〜テスト工程を担当。<br/>３年目から金融企業向けにSalesforce.comの導入支援・開発業務を担当。
+            </p>
+          </div>
+          <h2 class="articleTtl">
+            Skills
+          </h2>
+          <div class="articleDescription description">
+            <p>
+              HTML / CSS / Sass / JavaScript / GSAP / jQuery / Ruby / Ruby on Rails / MySQL / VB,VBA / MySQL / SQLServer
+            </p>
+          </div>
+          <h2 class="articleTtl">
+            Media
+          </h2>
+          <div class="articleDescription description">
+            <h4>Qiita</h4>
+            <a href="https://qiita.com/nozomi53motomachi">
+              https://qiita.com/nozomi53motomachi
+            </a>
+            <h4>GitHub</h4>
+            <a href="https://github.com/wish911wish">
+              https://github.com/wish911wish
+            </a>
+          </div>
+          <h2 class="articleTtl">
+            Contact
+          </h2>
+          <div class="articleDescription description">
+            <p>
+              wish911wish@gmail.com
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
