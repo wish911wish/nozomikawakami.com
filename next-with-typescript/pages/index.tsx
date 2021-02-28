6dfa957e5852d1ae319e45fcb6f2a798ddb838f8
wish911wish <wish911wish@gmail.com>
2021-02-28 14:50:08 +0900

    fix index page

--- next-with-typescript/pages/index.tsx
+++ next-with-typescript/pages/index.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head'
 export const Home = (): JSX.Element => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>Create Next App With Typescript Eslint Jest</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
