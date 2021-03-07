6c2b3c54a9eb4c4c7ef72ea3e328d477e7537be0
wish911wish <wish911wish@gmail.com>
2021-03-07 18:54:51 +0900

    add three

---
+++ next-with-typescript/interface/interface.ts
@@ -0,0 +1,11 @@
+export interface GeometryArg {
+  color: string
+  args: [
+    width?: number,
+    height?: number,
+    depth?: number,
+    widthSegments?: number,
+    heightSegments?: number,
+    depthSegments?: number
+  ]
+}
