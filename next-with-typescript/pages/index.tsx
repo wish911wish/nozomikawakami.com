fa7964621c7e1fb0e03959189f0246b8bf0ef5cd
wish911wish <wish911wish@gmail.com>
2021-03-21 12:39:19 +0900

    fix index page

--- next-with-typescript/pages/index.tsx
+++ next-with-typescript/pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import * as THREE from 'three'
 import { Canvas } from 'react-three-fiber'
 import React, { useEffect } from 'react'
@@ -7,24 +6,24 @@ import { GeometryArg, RandomData } from '../interface/interface'
 
 const number = 35
 const colors = [
-  '#A2CCB6',
-  '#FCEEB5',
-  '#EE786E',
-  '#e0feff',
-  'lightpink',
-  'lightblue',
+  '#F3F4F6',
+  '#E5E7EB',
+  '#D1D5DB',
+  '#9CA3AF',
+  '#6B7280',
+  '#4B5563',
 ]
 const random = (i: number): RandomData => {
   const r = Math.random()
 
   return {
     position: new THREE.Vector3(
-      100 - Math.random() * 200,
-      100 - Math.random() * 200,
-      i * 1.5
+      200 - Math.random() * 400,
+      200 - Math.random() * 400,
+      i * 1.5 //52.5 ~ 1.5の間になるように
     ),
     color: colors[Math.round(Math.random() * (colors.length - 1))],
-    scale: [1 + r * 14, 1 + r * 14, 1],
+    scale: [2 + r * 3, 2 + r * 3, 0.5],
     rotation: [0, 0, THREE.MathUtils.degToRad(Math.round(Math.random()) * 45)],
   }
 }
@@ -85,220 +84,384 @@ const Lights = (): JSX.Element => {
   )
 }
 
-export const Home = (): JSX.Element => (
-  <>
-    <Canvas shadowMap camera={{ position: [0, 0, 100], fov: 100 }}>
-      <Lights />
-      <Content />
-    </Canvas>
-    <div className="container">
-      <Head>
-        <title>Create Next App With Typescript Eslint Jest</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export const Home = (): JSX.Element => {
+  const worksRef = React.createRef<HTMLDivElement>()
+  const scrollToWorks = () => {
+    worksRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  }
 
-      <main>
-        <h1 className="title bg-red-400">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+  return (
+    <>
+      <div className="w-screen h-screen z-0 fixed">
+        <Canvas shadowMap camera={{ position: [0, 0, 100], fov: 100 }}>
+          <Lights />
+          <Content />
+        </Canvas>
+      </div>
+      <div className="z-10 relative">
+        <div className="container">
+          <main>
+            <h1 className="title bg-red-400">
+              Welcome to <a href="https://nextjs.org">Next.js!</a>
+            </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.tsx</code>
-        </p>
-
-        <button
-          onClick={() => {
-            window.alert('With typescript and Jest')
-          }}
-        >
-          Test Button
-        </button>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+            <p className="description">
+              Get started by editing <code>pages/index.tsx</code>
             </p>
-          </a>
+
+            <button
+              onClick={() => {
+                window.alert('With typescript and Jest')
+              }}
+            >
+              Test Button
+            </button>
+
+            <div className="grid">
+              <div className="card" onClick={scrollToWorks}>
+                <h3>スクロールする &rarr;</h3>
+                <p>スクロールしますよ。</p>
+              </div>
+              <a href="https://nextjs.org/docs" className="card">
+                <h3>Documentation &rarr;</h3>
+                <p>Find in-depth information about Next.js features and API.</p>
+              </a>
+
+              <a href="https://nextjs.org/learn" className="card">
+                <h3>Learn &rarr;</h3>
+                <p>
+                  Learn about Next.js in an interactive course with quizzes!
+                </p>
+              </a>
+
+              <a
+                href="https://github.com/vercel/next.js/tree/master/examples"
+                className="card"
+              >
+                <h3>Examples &rarr;</h3>
+                <p>Discover and deploy boilerplate example Next.js projects.</p>
+              </a>
+
+              <a
+                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                className="card"
+              >
+                <h3>Deploy &rarr;</h3>
+                <p>
+                  Instantly deploy your Next.js site to a public URL with
+                  Vercel.
+                </p>
+              </a>
+            </div>
+
+            <h1 className="title bg-red-400" ref={worksRef}>
+              Welcome to <a href="https://nextjs.org">Next.js!</a>
+            </h1>
+
+            <p className="description">
+              Get started by editing <code>pages/index.tsx</code>
+            </p>
+
+            <button
+              onClick={() => {
+                window.alert('With typescript and Jest')
+              }}
+            >
+              Test Button
+            </button>
+
+            <div className="grid">
+              <a href="https://nextjs.org/docs" className="card">
+                <h3>Documentation &rarr;</h3>
+                <p>Find in-depth information about Next.js features and API.</p>
+              </a>
+
+              <a href="https://nextjs.org/learn" className="card">
+                <h3>Learn &rarr;</h3>
+                <p>
+                  Learn about Next.js in an interactive course with quizzes!
+                </p>
+              </a>
+
+              <a
+                href="https://github.com/vercel/next.js/tree/master/examples"
+                className="card"
+              >
+                <h3>Examples &rarr;</h3>
+                <p>Discover and deploy boilerplate example Next.js projects.</p>
+              </a>
+
+              <a
+                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                className="card"
+              >
+                <h3>Deploy &rarr;</h3>
+                <p>
+                  Instantly deploy your Next.js site to a public URL with
+                  Vercel.
+                </p>
+              </a>
+            </div>
+
+            <h1 className="title bg-red-400">
+              Welcome to <a href="https://nextjs.org">Next.js!</a>
+            </h1>
+
+            <p className="description">
+              Get started by editing <code>pages/index.tsx</code>
+            </p>
+
+            <button
+              onClick={() => {
+                window.alert('With typescript and Jest')
+              }}
+            >
+              Test Button
+            </button>
+
+            <div className="grid">
+              <a href="https://nextjs.org/docs" className="card">
+                <h3>Documentation &rarr;</h3>
+                <p>Find in-depth information about Next.js features and API.</p>
+              </a>
+
+              <a href="https://nextjs.org/learn" className="card">
+                <h3>Learn &rarr;</h3>
+                <p>
+                  Learn about Next.js in an interactive course with quizzes!
+                </p>
+              </a>
+
+              <a
+                href="https://github.com/vercel/next.js/tree/master/examples"
+                className="card"
+              >
+                <h3>Examples &rarr;</h3>
+                <p>Discover and deploy boilerplate example Next.js projects.</p>
+              </a>
+
+              <a
+                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                className="card"
+              >
+                <h3>Deploy &rarr;</h3>
+                <p>
+                  Instantly deploy your Next.js site to a public URL with
+                  Vercel.
+                </p>
+              </a>
+            </div>
+
+            <h1 className="title bg-red-400">
+              Welcome to <a href="https://nextjs.org">Next.js!</a>
+            </h1>
+
+            <p className="description">
+              Get started by editing <code>pages/index.tsx</code>
+            </p>
+
+            <button
+              onClick={() => {
+                window.alert('With typescript and Jest')
+              }}
+            >
+              Test Button
+            </button>
+
+            <div className="grid">
+              <a href="https://nextjs.org/docs" className="card">
+                <h3>Documentation &rarr;</h3>
+                <p>Find in-depth information about Next.js features and API.</p>
+              </a>
+
+              <a href="https://nextjs.org/learn" className="card">
+                <h3>Learn &rarr;</h3>
+                <p>
+                  Learn about Next.js in an interactive course with quizzes!
+                </p>
+              </a>
+
+              <a
+                href="https://github.com/vercel/next.js/tree/master/examples"
+                className="card"
+              >
+                <h3>Examples &rarr;</h3>
+                <p>Discover and deploy boilerplate example Next.js projects.</p>
+              </a>
+
+              <a
+                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                className="card"
+              >
+                <h3>Deploy &rarr;</h3>
+                <p>
+                  Instantly deploy your Next.js site to a public URL with
+                  Vercel.
+                </p>
+              </a>
+            </div>
+          </main>
+
+          <footer>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Powered by{' '}
+              {/* <Image src="/vercel.svg" alt="Vercel Logo" height={'32'} width={'64'} /> */}
+            </a>
+          </footer>
+
+          <style jsx>{`
+            .container {
+              min-height: 100vh;
+              padding: 0 0.5rem;
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+            }
+
+            main {
+              padding: 5rem 0;
+              flex: 1;
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+            }
+
+            footer {
+              width: 100%;
+              height: 100px;
+              border-top: 1px solid #eaeaea;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+
+            footer img {
+              margin-left: 0.5rem;
+            }
+
+            footer a {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+
+            a {
+              color: inherit;
+              text-decoration: none;
+            }
+
+            .title a {
+              color: #0070f3;
+              text-decoration: none;
+            }
+
+            .title a:hover,
+            .title a:focus,
+            .title a:active {
+              text-decoration: underline;
+            }
+
+            .title {
+              margin: 0;
+              line-height: 1.15;
+              font-size: 4rem;
+            }
+
+            .title,
+            .description {
+              text-align: center;
+            }
+
+            .description {
+              line-height: 1.5;
+              font-size: 1.5rem;
+            }
+
+            code {
+              background: #fafafa;
+              border-radius: 5px;
+              padding: 0.75rem;
+              font-size: 1.1rem;
+              font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+                DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New,
+                monospace;
+            }
+
+            .grid {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              flex-wrap: wrap;
+
+              max-width: 800px;
+              margin-top: 3rem;
+            }
+
+            .card {
+              margin: 1rem;
+              flex-basis: 45%;
+              padding: 1.5rem;
+              text-align: left;
+              color: inherit;
+              text-decoration: none;
+              border: 1px solid #eaeaea;
+              border-radius: 10px;
+              transition: color 0.15s ease, border-color 0.15s ease;
+            }
+
+            .card:hover,
+            .card:focus,
+            .card:active {
+              color: #0070f3;
+              border-color: #0070f3;
+            }
+
+            .card h3 {
+              margin: 0 0 1rem 0;
+              font-size: 1.5rem;
+            }
+
+            .card p {
+              margin: 0;
+              font-size: 1.25rem;
+              line-height: 1.5;
+            }
+
+            @media (max-width: 600px) {
+              .grid {
+                width: 100%;
+                flex-direction: column;
+              }
+            }
+          `}</style>
+
+          <style jsx global>{`
+            html,
+            body {
+              padding: 0;
+              margin: 0;
+              font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                sans-serif;
+            }
+
+            * {
+              box-sizing: border-box;
+            }
+          `}</style>
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          {/* <Image src="/vercel.svg" alt="Vercel Logo" height={'32'} width={'64'} /> */}
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  </>
-)
+      </div>
+    </>
+  )
+}
 
 export default Home
