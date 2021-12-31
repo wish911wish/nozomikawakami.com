16594a80c363add05f762169570e970292deee53
wish911wish <wish911wish@users.noreply.github.com>
2021-12-31 11:54:38 +0000

    deploy: a6f72577dc724d5962c860e7f0651eb82698fcca

---
+++ _next/static/xW8GG6FS7S9VBwMSncUkp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b282b959608f58d4.css","static/chunks/pages/index-b76c6a46e6190e12.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
