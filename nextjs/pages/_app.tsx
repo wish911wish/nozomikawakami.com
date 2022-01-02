15feb1128867d40d924a187660151a11e027d4c9
wish911wish <wish911wish@gmail.com>
2022-01-02 16:27:10 +0900

    add fonts

--- nextjs/pages/_app.tsx
+++ nextjs/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
