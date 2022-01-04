b9322470f328f4c7bd784af823ae955f79f0cef3
wish911wish <wish911wish@gmail.com>
2022-01-04 22:48:21 +0900

    fix works page

--- nextjs/src/pages/works.tsx
+++ nextjs/src/pages/works.tsx
@@ -1,8 +1,12 @@
+import React from "react";
 import type { NextPage } from "next";
+import Image from "components/atoms/Image";
 import { css } from "@emotion/react";
 import Heading2 from "components/atoms/heading2";
 import Heading4 from "components/atoms/heading4";
 import Layout from "components/template/layout";
+import Work from "interfaces/work";
+import { getWorks } from "api/works";
 
 const title = css({
   height: "50vh",
@@ -14,11 +18,34 @@ const title = css({
 });
 
 const Home: NextPage = () => {
+  const [works, setWorks] = React.useState<Work[]>([]);
+
+  React.useEffect(() => {
+    (async () => {
+      const worksData = await getWorks();
+      setWorks(worksData);
+    })();
+  }, []);
+
   return (
     <Layout>
       <div css={title}>
         <Heading2 text="Works" />
         <Heading4 text="My works." />
+      </div>
+      <div>
+        {works.map((w) => (
+          <div key={w.id}>
+            <p>
+              {w.name} at{" "}
+              {`${w.periodStart.getFullYear()}/${
+                w.periodStart.getMonth() + 1
+              }/${w.periodStart.getDate()}`}
+            </p>
+            <Image alt={w.name} src={w.imageUrl} width={300} height={200} />
+          </div>
+        ))}
+        <div></div>
       </div>
     </Layout>
   );
