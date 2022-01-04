6ae1d315ae5565b9c86f42817c66ccff8f5a45c8
wish911wish <wish911wish@gmail.com>
2022-01-04 22:48:05 +0900

    make interface

---
+++ nextjs/src/interfaces/work.ts
@@ -0,0 +1,13 @@
+interface Work {
+  id: number;
+  imageUrl: string;
+  name: string;
+  periodEnd: Date;
+  periodStart: Date;
+  role: string;
+  summary: string;
+  technicalSkills: string;
+  url: string;
+}
+
+export default Work;
