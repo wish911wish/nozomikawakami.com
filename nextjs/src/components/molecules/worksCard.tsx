fcb0ab7ee8eb959d905256795eb4561e0737149b
wish911wish <wish911wish@gmail.com>
2022-01-09 22:19:36 +0900

    make works card components

---
+++ nextjs/src/components/molecules/worksCard.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { css } from "@emotion/react";
+import Work from "interfaces/work";
+import Image from "components/atoms/Image";
+
+const style = css({
+  fontSize: 14,
+  lineHeight: 1,
+  padding: [8, 16],
+});
+
+type Props = {
+  work: Work;
+};
+
+const worksCard = css({
+  width: "100%",
+});
+
+const WorksCard: React.FC<Props> = (props: Props) => {
+  const start = `${props.work.periodStart.getFullYear()}/${
+    props.work.periodStart.getMonth() + 1
+  }/${props.work.periodStart.getDate()}`;
+  return (
+    <div css={worksCard}>
+      <Image
+        alt={props.work.name}
+        src={props.work.imageUrl}
+        width={300}
+        height={200}
+      />
+      <p>
+        {props.work.name} at {start}
+      </p>
+    </div>
+  );
+};
+
+export default WorksCard;
