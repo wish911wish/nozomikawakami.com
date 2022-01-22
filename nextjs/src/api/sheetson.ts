8ed5723ec9a8348c143a443d7a59a0612c26abcb
wish911wish <wish911wish@gmail.com>
2022-01-22 21:24:31 +0900

    fix get index

--- nextjs/src/api/sheetson.ts
+++ nextjs/src/api/sheetson.ts
@@ -7,7 +7,13 @@ interface IndexResponse {
 }
 
 export const getIndex = async (sheetName: string) => {
-  const result: IndexResponse = await GET();
+  const result: IndexResponse = await GET(
+    `https://api.sheetson.com/v2/sheets/${sheetName}`,
+    {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_SHEETSON_API_KEY}`,
+      "X-Spreadsheet-Id": process.env.NEXT_PUBLIC_SHEETSON_SHEET_ID,
+    }
+  );
 
   return result.results;
 };
