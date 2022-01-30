345d790cba9d692be04c89cbff8dbee5216f4010
wish911wish <wish911wish@gmail.com>
2022-01-30 10:56:03 +0900

    fix works style

--- nextjs/src/pages/works.tsx
+++ nextjs/src/pages/works.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import type { NextPage } from "next";
-import Image from "components/atoms/Image";
 import { css } from "@emotion/react";
 import Heading2 from "components/atoms/heading2";
 import Heading4 from "components/atoms/heading4";
@@ -9,6 +8,10 @@ import Layout from "components/template/layout";
 import Work from "interfaces/work";
 import { getWorks } from "api/works";
 
+const mainVisual = css({
+  height: "100vh",
+});
+
 const title = css({
   height: "50vh",
   display: "flex",
@@ -16,6 +19,26 @@ const title = css({
   alignItems: "center",
   justifyContent: "end",
   rowGap: 14,
+});
+
+const scroll = css({
+  height: "25vh",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "end",
+});
+
+const arrow = css({
+  position: "absolute",
+  width: 24,
+  height: 24,
+  marginLeft: -12,
+  borderLeft: "3px solid #2c3e50",
+  borderBottom: "3px solid#2c3e50",
+  transform: "rotate(-45deg)",
+  animation: "sdb 1.5s infinite",
+  boxSizing: "border-box",
 });
 
 const worksList = css({
@@ -42,9 +65,15 @@ const Home: NextPage = () => {
 
   return (
     <Layout>
-      <div css={title}>
-        <Heading2 text="Works" />
-        <Heading4 text="My works." />
+      <div css={mainVisual}>
+        <div css={title}>
+          <Heading2 text="Works" />
+          <Heading4 text="My works." />
+        </div>
+        <div css={scroll}>
+          <p>scroll</p>
+          <a className="arrow"></a>
+        </div>
       </div>
       <div css={worksList}>
         {works.map((w) => (
