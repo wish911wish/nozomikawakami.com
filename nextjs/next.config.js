faf9829af83ce00986c5b1e07f24f19c6d860d92
wish911wish <wish911wish@gmail.com>
2022-03-13 12:56:55 +0900

    add font file

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    loader: 'custom'
-  }  
-}
+    loader: "custom",
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+      type: "asset",
+    });
+    return config;
+  },
+};
